b2e9845dd980170e31d1808070a738167688716b
rayyan <rayyan10@bellsouth.net>
2021-02-07 13:13:52 -0600

    📦  FEAT: implement shunting yard algorithm

--- lib/practice/calc.ex
+++ lib/practice/calc.ex
@@ -10,9 +10,11 @@ defmodule Practice.Calc do
     expr
     |> String.split(~r/\s+/) # splitting on all whitespace here
     # |> Enum.chunk_by(fn(c) -> String.contains?(c, ["+", "-"]) end)
-    |> hd
-    |> parse_float
-    |> :math.sqrt()
+    # |> hd
+    |> shuntingYard([], [])
+    # |> inspect
+    # |> parse_float
+    # |> :math.sqrt()
 
     # Hint:
     # expr
@@ -21,5 +23,32 @@ defmodule Practice.Calc do
     # |> convert to postfix
     # |> reverse to prefix
     # |> evaluate as a stack calculator using pattern matching
+  end
+
+  defp number?(strTok) do
+    Float.parse(strTok) != :error
+  end
+
+  defp operator?(strTok) do
+    String.contains?("+-*/", strTok) && String.length(strTok) == 1
+  end
+
+  defp greaterThanEqPrecedence?(thisOperator, thatOperator) do
+    precedence = %{"+" => 1, "-" => 1, "*" => 2, "/" => 2}
+    precedence[thisOperator] <= precedence[thatOperator]
+  end
+
+  defp shuntingYard(exprArr, output, stack) do
+    inspect(exprArr)
+    cond do
+      length(exprArr) == 0 -> output ++ stack
+      number?(hd(exprArr)) -> shuntingYard(tl(exprArr), output ++ [hd(exprArr)], stack)
+      operator?(hd(exprArr)) -> opsGreaterThanEqPrecedence = Enum.take_while(stack,
+                                  fn(operator) -> greaterThanEqPrecedence?(hd(exprArr), operator) end)
+                                shuntingYard(tl(exprArr),
+                                             output ++ opsGreaterThanEqPrecedence,
+                                             [hd(exprArr) | Enum.drop(stack, length(opsGreaterThanEqPrecedence))])
+      # TODO: catch all case
+    end
   end
 end
